91ea70c2877e90adb9aba6aedb2a45bbefc65fa1
AldricGF <aldricgutierrezferreid@gmail.com>
2020-05-21 05:35:00 +0200

    Added finished P4

---
+++ Panoramic-Roll-a-ball_Aldric_Gutierrez_Ferre_205160/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text winText2;
+    public Text timeText;
+
+    public float totalTime;
+
+    private Rigidbody rb;
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 8;
+        SetCountText();
+        winText.text = "";
+        winText2.text = "";
+        totalTime = 0;
+        timeText.text = "";
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement*speed);
+        rb.AddForce(-rb.velocity); //remove inertia
+
+        totalTime += Time.deltaTime;
+        timeText.text = "Current time: " + totalTime + "s";
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count - 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Remaining: " + count.ToString();
+        if(count <= 0){
+            winText.text = "You won in " + totalTime + "s !!!";
+            winText2.text = "You won in " + totalTime + "s !!!";
+        }
+    }
+}
---
+++ Panoramic-Roll-a-ball_Aldric_Gutierrez_Ferre_205160/Assets/Scripts/RandomMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMove : MonoBehaviour
+{
+    public float speed2;
+
+    private float totalTime2;
+
+    private float moveHorizontal;
+    private float moveVertical;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        totalTime2 = 0;
+
+        moveHorizontal = Random.Range(-1.0f, 1.0f);
+        moveVertical = Random.Range(-1.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    	if(totalTime2 > 0.5){
+    		totalTime2 = 0;
+    		moveHorizontal = Random.Range(-1.0f, 1.0f);
+        	moveVertical = Random.Range(-1.0f, 1.0f);
+
+    	}
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement*speed2);
+        rb.AddForce(-rb.velocity*5); //remove inertia
+
+        totalTime2 += Time.deltaTime;
+    }
+}
